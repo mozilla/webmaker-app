907bb7660c0f0c643f99fcf2b03532e586f6cb1c
Jordan Theriault <jordantheriault@gmail.com>
2014-12-04 00:40:07 -0500

    Map working on new and reloaded pages, some visual issues on mobile device due to edit mode

--- blocks/map/index.js
+++ blocks/map/index.js
@@ -1,3 +1,5 @@
+var map = null;
+
 module.exports = {
     className: 'map',
     template: require('./index.html'),
@@ -5,30 +7,64 @@ module.exports = {
         name: 'Map',
         icon: '/images/blocks_map.png',
         attributes: {
+            locationName: {
+                label: 'Location Name',
+                type: 'string',
+                value: 'Mozilla'
+            },
             latitude: {
                 label: 'Latitude',
-                type: 'double',
-                value: '0.0'
+                type: 'number',
+                value: '37.387444'
             },
             longitude: {
                 label: 'Longitude',
-                type: 'double',
-                value: '0.0'
+                type: 'number',
+                value: '-122.061019'
             },
             address: {
                 label: 'Address',
                 type: 'string',
-                value: 'The Pond Road',
+                value: '321 E Evelyn Ave, Mountain View, CA, 94041',
             },
         }
     },
     attached: function () {
-        var L = require('leaflet');
-        L.Icon.Default.imagePath = '../../node_modules/leaflet/dist/images/';
-        var map = L.map('map').setView([43.07265,-89.400929], 10);
         
+
+        //Grab bindings
+        self = this;
+        var L = require('leaflet');
+        L.Icon.Default.imagePath = "../../node_modules/leaflet/dist/images";
+
+
+        //Grab location
+        var loc = new L.latLng(parseFloat(self.$data.attributes.latitude.value), parseFloat(self.$data.attributes.longitude.value));
+
+        //Remove map if it's already drawn
+        if(map != null)
+            map.remove();
+
+
+        //Set map view
+        map = L.map('map').setView(loc, 16);
+
+        //Center when popup marker is placed
+        map.on('popupopen', function(e) {
+            var px = map.project(e.popup._latlng);
+            px.y -= e.popup._container.clientHeight/2 
+            map.panTo(map.unproject(px),{animate: true}); 
+        });
+
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
+        
+        //Create popup marker
+        var marker = L.marker(loc).addTo(map);
+        marker.bindPopup("<p> <strong>" + self.$data.attributes.locationName.value + "</strong> <br />" + self.$data.attributes.address.value + "</p>"
+                        ).openPopup();
+
+
     }
 };
