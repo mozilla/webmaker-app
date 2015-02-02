0843cc4bbcd5c4ab79312c7ba31f6719250a1777
Aki Rose Braun <hi@akibraun.com>
2015-02-02 10:29:53 -0800

    Fixes #833:  Add animation to icon chooser

--- views/make/index.js
+++ views/make/index.js
@@ -60,23 +60,36 @@ module.exports = view.extend({
             this.selectIconImage('next');
         },
         selectIconImage: function (direction) {
+          var self = this;
+          var data = self.$data;
+          var index = data.iconImages.indexOf(data.app.iconImage) || 0;
+          var maxIndex = data.iconImages.length - 1;
 
-            var data = this.$data;
-            var index = data.iconImages.indexOf(data.app.iconImage) || 0;
-            var maxIndex = data.iconImages.length - 1;
+          self.$el.querySelector('#icon-image').className = 'icon-image';
 
-            direction === 'next' ? index++ : index--;
+          setTimeout(function () {
+            if (direction === 'next') {
+              self.$el.querySelector('#icon-image').classList.add('right');
+              index++;
+            } else {
+              self.$el.querySelector('#icon-image').classList.add('left');
+              index--;
+            }
             if (index < 0) {
-                index = maxIndex;
+              index = maxIndex;
             }
             if (index > maxIndex) {
-                index = 0;
+              index = 0;
             }
-            app.update({
-                iconImage: data.iconImages[index]
-            });
+          }, 10);
 
-            this.$data.currentIconIndex = index;
+          setTimeout(function () {
+            app.update({
+              iconImage: data.iconImages[index]
+            });
+          }, 100);
+
+          self.$data.currentIconIndex = index;
         },
         onSelectIconColor: function (color) {
             app.update({
