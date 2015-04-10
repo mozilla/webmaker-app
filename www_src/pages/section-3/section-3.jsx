d1420b37394fdd1de6561c832278b78134f83f64
Kate Hudson <k88hudson@gmail.com>
2015-04-10 14:47:36 -0400

    use native browser stuff

--- www_src/pages/section-3/section-3.jsx
+++ www_src/pages/section-3/section-3.jsx
@@ -1,12 +1,34 @@
 var React = require('react');
 var render = require('../../lib/render.jsx');
 
+var fakeBlocks = [];
+for (var i = 0; i < 50; i++) {
+  fakeBlocks.push(i);
+}
+
+var Tile = React.createClass({
+  render: function () {
+    return (<div className="project-tile">
+      {this.props.text}
+    </div>);
+  }
+});
+
 var Three = React.createClass({
-  contextTypes: {
-    router: React.PropTypes.func
+  getInitialState: function () {
+    return {};
+  },
+  componentDidMount: function () {
+    this.setState({
+      scrollX: window.innerWidth / 2,
+      scrollY: window.innerHeight / 2
+    });
   },
   render: function () {
-    return <div>This is section 3</div>
+    window.scrollTo(this.state.scrollX, this.state.scrollY);
+    return <div className="panner">
+      {fakeBlocks.map(block => <Tile text={block} />)}
+    </div>
   }
 });
 
