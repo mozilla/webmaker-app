56ab07c66189d031b2b210917d59e7443ab1f55d
Pomax <pomax@nihongoresources.com>
2015-04-13 21:19:42 -0700

    lollerskates

--- www_src/pages/section-3/section-3.jsx
+++ www_src/pages/section-3/section-3.jsx
@@ -22,6 +22,8 @@ var Three = React.createClass({
     return (<div className="blockedit">
       <p>Let's try some stuff eh?</p>
       <div className="controls">
+        <button onClick={this.loadFromString}>Load...</button>
+        <button onClick={this.saveToString}>Save...</button>
         <button onClick={this.addHeader}>Header</button>
         <button onClick={this.addText}>Text</button>
         <button onClick={this.addImage}>Image</button>
@@ -38,8 +40,13 @@ var Three = React.createClass({
       if(m.type==="text")  { element = <TextField value={m.value} />; }
       if(m.type==="header"){ element = <Header value={m.value} />; }
       if(m.type==="image") { element = <Image src={m.src} alt={m.alt} />; }
-      return <div><Positionable>{element}</Positionable></div>;
+      var positionable = <Positionable clickHandler={this.clickHandler}>{element}</Positionable>;
+      return <div>{positionable}</div>;
     });
+  },
+
+  clickHandler: function(positionable) {
+    positionable.toggle();
   },
 
   append: function(obj) {
@@ -68,6 +75,32 @@ var Three = React.createClass({
       src: "https://farm4.staticflickr.com/3609/3567927108_8300f83fab_b.jpg",
       alt: "This is an image"
     });
+  },
+
+  save: function() {
+    return JSON.stringify(this.state.content);
+  },
+
+  saveToString: function() {
+    prompt("Content data:", this.save())
+  },
+
+  load: function(content) {
+    this.setState({
+      content: content
+    }, function() {
+      console.log("restored state");
+    });
+  },
+
+  loadFromString: function() {
+    var data = prompt("Content data:");
+    try {
+      var content = JSON.parse(data);
+      this.load(content);
+    } catch (e) {
+      alert("could not parse data as JSON");
+    }
   }
 });
 
