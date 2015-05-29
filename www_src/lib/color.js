1680cb4c76d8568ac8c8476140c78206344b2364
Andrew Sliwinski <andrewsliwinski@acm.org>
2015-05-28 22:15:28 -0700

    Hook up RGBA sliders in tinker mode. Re GH-1729

--- www_src/lib/color.js
+++ www_src/lib/color.js
@@ -1,17 +1,51 @@
 var Color = require('color');
-var minimumContrast = 2;
 
 module.exports = {
-  getContrastingColor: (function() {
-    var white = Color().rgb(255, 255, 255),
-        whiteCSS = white.rgbString(),
-        black = Color().rgb(0, 0, 0),
-        blackCSS = black.rgbString();
-    return function(input) {
-      var c = Color(input),
-          wc = white.contrast(c),
-          bc = black.contrast(c);
-      return (wc < bc && wc >= minimumContrast) ? whiteCSS : blackCSS;
-    };
-  }())
+  /**
+   * Converts RGB color into a HEX string
+   *
+   * @param  {int} r Red
+   * @param  {int} g Green
+   * @param  {int} b Blue
+   *
+   * @return {string}
+   */
+  rgbToHex: function (r, g, b) {
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  },
+
+  /**
+   * Converts RGBA color into an "rgba()" CSS string
+   *
+   * @param  {int} r Red
+   * @param  {int} g Green
+   * @param  {int} b Blue
+   * @param  {float} a ALpha
+   *
+   * @return {string}
+   */
+  rgbaToCss: function (r, g, b, a) {
+    return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+  },
+
+  /**
+   * Calculates a contrasting color.
+   *
+   * @param  {string} input Reference (hex) color value
+   *
+   * @return {string}
+   */
+  getContrastingColor: function (input) {
+    var white = Color().rgb(255, 255, 255);
+    var whiteCSS = white.rgbString();
+    var black = Color().rgb(0, 0, 0);
+    var blackCSS = black.rgbString();
+    var minimumContrast = 2;
+
+    var c = Color(input);
+    var wc = white.contrast(c);
+    var bc = black.contrast(c);
+
+    return (wc < bc && wc >= minimumContrast) ? whiteCSS : blackCSS;
+  }
 };
--- www_src/pages/tinker/tinker.jsx
+++ www_src/pages/tinker/tinker.jsx
@@ -1,66 +1,68 @@
-var React = require('react');
+var React = require('react/addons');
 
+var color = require('../../lib/color.js');
 var render = require('../../lib/render.jsx');
-
-var Tabs = require('../../components/tabs/tabs.jsx');
-var ColorSpectrum = require('../../components/color-spectrum/color-spectrum.jsx');
+var router = require('../../lib/router.jsx');
 var Slider = require('../../components/range/range.jsx');
 
 var Tinker = React.createClass({
+  mixins: [
+    React.addons.LinkedStateMixin,
+    router
+  ],
   getInitialState: function () {
     return {
-      color: '#8FB70A'
+      red: 200,
+      green: 100,
+      blue: 50,
+      alpha: 1
     };
   },
-  onChangeColor: function (color) {
-    this.setState({
-      color: color
-    });
-  },
   render: function () {
-    var tabs = [
-      {
-        menu: <img className="icon" src="../../img/pencil.svg" />,
-        body: (<div>
-          <div className="form-group">
-            <ColorSpectrum value={this.state.color} onChange={this.onChangeColor} />
-          </div>
-          <div className="form-group">
-            <label>Transparency</label>
-            <Slider />
-          </div>
-        </div>)
-      },
-      {
-        menu: <img className="icon" src="../../img/settings.svg" />,
-        body: (<div>
-          {['Red', 'Green', 'Blue'].map((color, i) => {
-            return (<div className="form-group">
-              <label>{color}</label>
-              <Slider max={255} unit="" />
-            </div>);
-          })}
-          <div className="form-group">
-            <label>Transparency</label>
-            <Slider />
-          </div>
-        </div>)
-      }
-    ];
     return (
-      <div id="tinker">
-        <div className="editor-preview">
-          <img src="../../img/toucan.svg" />
+      <div id='tinker'>
+        <div className='editor-preview'>
+          <img src='../../img/toucan.svg' />
         </div>
-        <div className="color-preview">
-          <code>color: {this.state.color};</code>
-          <div className="color-preview-right">
-            <div style={{backgroundColor: this.state.color}} className="color-preview-swatch" />
+        
+        <div className='color-preview'>
+          <code>color: {this.generateColorString()};</code>
+          <div className='color-preview-right'>
+            <div style={{backgroundColor: this.generateColorString()}} className='color-preview-swatch' />
           </div>
         </div>
-        <Tabs className="editor-options" tabs={tabs} />
+
+        <div className='editor-options'>
+          <div className='form-group'>
+            <label>Red</label>
+            <Slider id='red' max={255} linkState={this.linkState} />
+
+            <label>Green</label>
+            <Slider id='green' max={255} linkState={this.linkState} />
+
+            <label>Blue</label>
+            <Slider id='blue' max={255} linkState={this.linkState} />
+
+            <label>Transparency</label>
+            <Slider id='alpha' max={1} step={0.01} linkState={this.linkState} />
+          </div>
+        </div>
       </div>
     );
+  },
+
+  /**
+   * Transform RGBA state into a valid CSS color.
+   *
+   * @return {string} HEX or RGBA color string
+   */
+  generateColorString: function () {
+    // If alpha is less than "1.0", use "rgba()" syntax
+    if (this.state.alpha < 1) {
+      return color.rgbaToCss(this.state.red, this.state.green, this.state.blue, this.state.alpha);
+    }
+    
+    return color.rgbToHex(this.state.red, this.state.green, this.state.blue);
   }
 });
 
