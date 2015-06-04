0b123788b1224d567bdfe8683abb93779eebec60
Pomax <pomax@nihongoresources.com>
2015-06-03 18:02:28 -0700

    docs

--- www_src/components/snackbar/snackbar.jsx
+++ www_src/components/snackbar/snackbar.jsx
@@ -14,17 +14,29 @@ var Snackbar = React.createClass({
     }
     return <div className={className}>{ this.state.message }</div>;
   },
+  /**
+   * Hide the snackbar from view
+   */
   hide: function() {
     this.setState({
       hidden: true
     });
   },
+  /**
+   * Public API: show a message using the snackbar notification
+   * system. This slides the message into view at the bottom of
+   * the screen following the "snackbar" pattern from Material
+   * Design, leaving the message up for 3 seconds before sliding
+   * it back out of view.
+   */
   showMessage: function(msg) {
     this.setState({
       message: msg,
       hidden: false
     }, function() {
-      // TODO: ideally this is an event, but CSS can't inform
+      // TODO: ideally this is an event, but the duration is going
+      //       to have to be hard coded *somewhere*, and it's probably
+      //       best to keep that here, rather than in LESS code.
       setTimeout(_ => this.hide(), 3000);
     });
   }
